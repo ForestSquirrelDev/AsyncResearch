fbc17e0741fb69c24676e7fd2ccc6e7b471521e9
Lev <cfire100500@gmail.com>
2026-02-14 19:11:08 +0300

    add task when any example

---
+++ AsyncExperiments/Chapter_7_TheArtOfLosingStacktrace/Source/TaskWhenAnyExample.cs
@@ -0,0 +1,37 @@
+﻿namespace AsyncResearch.AsyncExperiments.Chapter_7_TheArtOfLosingStacktrace.Source
+{
+    public static class TaskWhenAnyExample
+    {
+        public static async Task WhenAnyExceptionsExample()
+        {
+            var t1 = Exception1();
+            var t2 = Exception2();
+            var t3 = Exception3();
+            
+            var any = Task.WhenAny(t1, t2, t3);
+            try
+            {
+                await any;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private static async Task Exception1()
+        {
+            throw new Exception("Exception1");
+        }
+
+        private static async Task Exception2()
+        {
+            throw new Exception("Exception2");
+        }
+
+        private static async Task Exception3()
+        {
+            throw new Exception("Exception3");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace AsyncResearch
     {
         public static async Task Main(string[] args)
         { 
-            await TaskWhenAllExample.WhenAllExceptionsExample();
+            await TaskWhenAnyExample.WhenAnyExceptionsExample();
         }
     }
 }
