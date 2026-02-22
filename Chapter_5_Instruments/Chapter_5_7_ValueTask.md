1f779dc8ceff3ef0a3458bbb59345f21ebbb6da5
Lev <cfire100500@gmail.com>
2026-02-22 23:56:20 +0300

    partially add: value task

---
+++ Chapter_5_Instruments/Chapter_5_7_ValueTask.md
@@ -0,0 +1,7 @@
+﻿Вместо `Task`, в методе можно вернуть `ValueTask`:
+````csharp
+public static ValueTask RunValueTaskExample()
+{
+    return ValueTask.CompletedTask;
+}
+````
---
+++ Chapter_5_Instruments/Source/ValueTaskExample.cs
@@ -0,0 +1,34 @@
+﻿namespace AsyncResearch.Chapter_5_Instruments.Source
+{
+    public static class ValueTaskExample
+    {
+        private static bool _calledOnce;
+        
+        public static async Task RunValueTaskExample()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var t = OptimizedDoWork();
+                await t;
+                await t;
+            }
+        }
+
+        private static ValueTask OptimizedDoWork()
+        {
+            if (!_calledOnce)
+            {
+                _calledOnce = true;
+                return new ValueTask(ShouldDoWorkOnce());
+            }
+            
+            return ValueTask.CompletedTask;
+        }
+
+        private static async Task ShouldDoWorkOnce()
+        {
+            await Task.Delay(100);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
